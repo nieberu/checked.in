64ca9ec528e1e7cd2ed3ab7498f66bf219f07524
Sathish Kumar Santhosam <sathish.kumar.santhosam@macys.com>
2020-06-27 19:47:53 -0400

    Adding Home Page

--- app/src/main/java/development/software/mobile/checkedin/MainActivity.java
+++ app/src/main/java/development/software/mobile/checkedin/MainActivity.java
@@ -1,8 +1,11 @@
 package development.software.mobile.checkedin;
 
 //import android.support.v7.app.AppCompatActivity;AppCompatActivity
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +22,10 @@ import development.software.mobile.checkedin.models.User;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView text;
+    // private TextView text;
     private FirebaseAuth mAuth;
+    Button login;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         User user = (User)getIntent().getSerializableExtra("User");
-        text = findViewById(R.id.mainText);
+        // text = findViewById(R.id.mainText);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference();
@@ -36,21 +41,43 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        myRef.child("users").child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
+        if(currentUser != null){
+            myRef.child("users").child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    userObject[0] = dataSnapshot.getValue(User.class);
+                    Log.d("JAY", "Value is: " + userObject[0]);
+                    //text.setText(userObject[0].toString());
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w("JAY", "Failed to read value.", error.toException());
+                }
+            });
+        }
+
+        login = (Button) findViewById(R.id.loginButton);
+        register = (Button) findViewById(R.id.registerButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                userObject[0] = dataSnapshot.getValue(User.class);
-                Log.d("JAY", "Value is: " + userObject[0]);
-                text.setText(userObject[0].toString());
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Registration.class);
+                startActivity(intent);
             }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("JAY", "Failed to read value.", error.toException());
-            }
         });
 
     }
