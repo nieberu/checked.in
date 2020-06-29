0e6a27be08e179dadbb13f486d9c1bbb77945193
Jordan Simo <jordansimo@Jordans-MacBook-Air.local>
2020-06-29 01:51:55 -0400

    Added profile picture upload

--- app/src/main/java/development/software/mobile/checkedin/Registration.java
+++ app/src/main/java/development/software/mobile/checkedin/Registration.java
@@ -1,23 +1,38 @@
 package development.software.mobile.checkedin;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
@@ -25,10 +40,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 import butterknife.ButterKnife;
 import butterknife.BindView;
-import butterknife.OnClick;
 import development.software.mobile.checkedin.models.User;
 
 public class Registration extends AppCompatActivity {
@@ -50,8 +70,20 @@ public class Registration extends AppCompatActivity {
     Button _signupButton;
     @BindView(R.id.link_login)
     TextView _loginLink;
+    @BindView(R.id.picture_button)
+    Button _pictureButton;
+    @BindView(R.id.picture_name)
+    TextView _pictureName;
+
+    private String imagePath = "";
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int MY_READ_EXTERNAL_STORAGE_REQUEST_CODE = 101;
+    private static final int MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 102;
+
+
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+    private StorageReference mStorageRef;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,12 +91,32 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         ButterKnife.bind(this);
         FirebaseApp.initializeApp(this);
-
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
 
         SpannableString text = new SpannableString("Already a member? Login");
         text.setSpan(new UnderlineSpan(), 18, 23, 0);
         _loginLink.setText(text);
+
+        _pictureButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+                }
+                else if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_READ_EXTERNAL_STORAGE_REQUEST_CODE);
+                }
+                else if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+                }
+                else{
+                    selectImage();
+                }
+                System.out.println(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+            }
+        });
 
         _signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +132,33 @@ public class Registration extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean flag = true;
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                flag = false;
+            }
+        }
+        if (requestCode == MY_READ_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                flag = false;
+            }
+        }
+        if (requestCode == MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            selectImage();
+        }
+        else {
+            Toast.makeText(this, "A or multiple permissions have been denied", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void signup() {
@@ -110,9 +189,13 @@ public class Registration extends AppCompatActivity {
     }
 
 
-    public void onSignupSuccess() {
+    public void onSignupSuccess(User userObject) {
         _signupButton.setEnabled(true);
         setResult(RESULT_OK, null);
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra("User", userObject);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Signup successful", Toast.LENGTH_LONG).show();
         finish();
     }
 
@@ -135,6 +218,13 @@ public class Registration extends AppCompatActivity {
             valid = false;
         } else {
             _firstNameText.setError(null);
+        }
+        if (imagePath.equals(null)) {
+            _pictureName.setError("Please select a profile picture");
+            valid = false;
+        }
+        else{
+            _pictureName.setError(null);
         }
 
         if (lastName.isEmpty() || lastName.length() < 3) {
@@ -175,22 +265,106 @@ public class Registration extends AppCompatActivity {
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             User userObject = new User(user.getUid(), firstName, lastName, email, username, phoneNumber);
-
+                            uploadProfilePic(imagePath, user.getUid());
                             myRef.child("users").child(user.getUid()).setValue(userObject);
 
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            intent.putExtra("User", userObject);
-                            startActivity(intent);
-//                            updateUI(user);
-                            Toast.makeText(getBaseContext(), "Signup successful", Toast.LENGTH_LONG).show();
+                            onSignupSuccess(userObject);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
                             Toast.makeText(getBaseContext(), "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
+                            onSignupFailed();
                         }
 
+                    }
+                });
+    }
+
+    private void selectImage(){
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery", "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(Registration.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo"))
+                {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    //intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1);
+                }
+                else if (options[item].equals("Choose from Gallery"))
+                {
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+                }
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                // CALL THIS METHOD TO GET THE URI FROM THE BITMAP
+                Uri tempUri = getImageUri(getApplicationContext(), photo);
+
+                // CALL THIS METHOD TO GET THE ACTUAL PATH
+                imagePath = getRealPathFromURI(tempUri);
+                _pictureName.setText(imagePath);
+            } else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                imagePath = getRealPathFromURI(selectedImage);
+                _pictureName.setText(imagePath);
+            }
+        }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        Bitmap OutImage = Bitmap.createScaledBitmap(inImage, 1000, 1000,true);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), OutImage, "temp", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        String path = "";
+        if (getContentResolver() != null) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+                int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                path = cursor.getString(idx);
+                cursor.close();
+            }
+        }
+        return path;
+    }
+
+    public void uploadProfilePic(String path, String uid){
+
+        Uri file = Uri.fromFile(new File(path));
+        StorageReference ppRef = mStorageRef.child("profilepictures/"+uid+"/pp.jpg");
+
+        ppRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+//                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
                     }
                 });
     }
